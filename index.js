7ee1b1916b7a43a23c6c15824356a5da7650f4ab
Lixel Amos <amosbett99@gmail.com>
2023-03-17 01:45:36 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("i want to bathe my dog");
+
+const mondayWork = function (work ="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("work from home");
+let wrapAdjective = function(sad="*") {
+    return function(word="special") {
+      return `You are ${sad}${word}${sad}!`
+    }
+  }
